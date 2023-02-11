a7c8b41d77a5ae0fa7ebb815bcd8ff622d0bdf50
Raman Krytski <rkeydevelop@gmail.com>
2023-02-11 09:14:58 +0100

    init: vite project initialisated

---
+++ readme.md
@@ -0,0 +1,5 @@
+# project lat test - 1
+
+<pre>
+npm create vite@latest
+</pre>
---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import './index.css'
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+            KEK
+        </div>
+    );
+};
+
+export default App;
