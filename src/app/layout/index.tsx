461c2d526d3f982df7a2a16b771d6f936e450fee
Raman Krytski <rkeydevelop@gmail.com>
2023-02-15 05:50:37 +0100

    fix: fixed import statement

--- src/app/layout/index.tsx
+++ src/app/layout/index.tsx
@@ -4,7 +4,7 @@ import Header from "./Header";
 import AsideLeft from "./Aside/AsideLeft";
 import AsideRight from "./Aside/AsideRight";
 import Footer from "./Footer";
-import Profile from "../../page/Profile";
+import {Profile} from "../../page/Profile";
 import Error from "../../page/Error";
 
 const Layout = () => {
